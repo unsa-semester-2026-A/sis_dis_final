b8df8bbcb0496410d756994ad634bc1f99a284df
RodrygoLeu <rodrygoleu7@gmail.com>
2026-06-27 14:59:45 -0500

    feature: variable handler

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -12,4 +12,5 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+  subscription_id = var.subscription_id
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "subscription_id" {
+  type        = string
+  description = "the azure subscription id"
+  sensitive   = true
+}
